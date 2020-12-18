311f949437944cbe9c7240d71be004b41e7c9689
aubry prieur <aubry.prieur@gmail.com>
2020-12-18 12:05:00 +0100

    implemented user authnetication with first name ansd last name, conditional logic for header, setted up flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
+import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,4 +17,6 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+global.toastr = require("toastr")
 
