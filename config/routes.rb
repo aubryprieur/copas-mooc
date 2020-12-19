197d72c908563982e4b0278f7e6b3d843d75a33d
aubry prieur <aubry.prieur@gmail.com>
2020-12-19 15:40:20 +0100

    Created admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   root "static#homepage"
   resources :workshops
+
+  namespace :admin do
+    resources :users
+    resources :workshops
+  end
+
 end
