d22008614980fbd49a2be19cb2b211599f0fcbc1
aubry prieur <aubry.prieur@gmail.com>
2020-12-19 16:18:45 +0100

    Login views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, :path => '', :path_names => {:sign_in => 'signin_as_admin', :sign_out => 'signout_as_admin'}, controllers: { sessions: 'admin/sessions' }
   devise_for :users
   root "static#homepage"
   resources :workshops
 
   namespace :admin do
+    root "static_pages#dashboard"
+    resources :static_pages
     resources :users
     resources :workshops
   end
