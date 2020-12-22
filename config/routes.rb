89ada3bbb6178ee7e730d8f8d710e8639ce82eb6
aubry prieur <aubry.prieur@gmail.com>
2020-12-22 11:27:34 +0100

    Created comment controller and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,31 @@
 Rails.application.routes.draw do
   devise_for :admins, :path => '', :path_names => {:sign_in => 'signin_as_admin', :sign_out => 'signout_as_admin'}, controllers: { sessions: 'admin/sessions' }
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   get "/edit_profile", to: "users#profile"
   get "/edit_avatar", to: "users#avatar"
+
   root "workshops#index"
 
   resources :users
-  resources :workshops
+  resources :workshops do
+    resources :lessons
+  end
   resources :lessons do
     collection do
       patch :sort
     end
   end
 
-  resources :workshops do
-    resources :lessons
+  resources :lessons do
+    resources :comments
+  end
+
+  resources :users do
+    resources :comments
   end
 
   namespace :admin do
