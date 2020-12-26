76b9fa52f6deb5e19147480cd795c0da6000b5ab
aubry prieur <aubry.prieur@gmail.com>
2020-12-26 17:26:44 +0100

    Updated views and create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Admin.destroy_all
+
+puts 'Creating admin...'
+
+Admin.create!(email: 'admin@example.com', password: 'AdminPassword', password_confirmation: 'AdminPassword')
+
+puts 'Creating user...'
+
+User.create!(email: 'aubry.prieur@copas.coop', password: 'UserPassword', password_confirmation: 'UserPassword')
+
+puts 'This is the end'
